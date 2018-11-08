8f0078f1c9059dac9e4ffa430601baf62f5d9ee9
yagelmeister <36314047+yagelmeister@users.noreply.github.com>
2018-11-08 16:48:27 +0100

    Add files via upload

---
+++ score.py
@@ -0,0 +1,27 @@
+"""
+Heuristieken UvA 2018-2019
+Lesroosters
+Johan Diepstraten, Ya'gel Schoonderbeek, Sam Verhezen
+
+Program assigns an score to schedule.
+"""
+
+from roostermaker import *
+
+# keep track of stats
+rooms = []
+courses = []
+score = 0
+
+# schedule
+total_schedule(rooms,courses)
+
+# adjusts score based on matrix
+def matrix_checker():
+    if True:
+        continue
+
+# adjust score based on students
+
+# return total score
+print(score)
