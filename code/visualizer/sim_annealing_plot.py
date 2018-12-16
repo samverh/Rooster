4c6c31819a6da0e90ac23c55e0fce8a0eac3cbcb
samverh <43989279+samverh@users.noreply.github.com>
2018-12-16 17:29:53 +0100

    plot visualizer data updated

--- code/visualizer/sim_annealing_plot.py
+++ code/visualizer/sim_annealing_plot.py
@@ -12,26 +12,51 @@ import pandas as pd
 import numpy as np
 import matplotlib.patches as mpatches
 
+# prompt user for input
+type = input("Which data do you want to visualize? (students inc, courses only): ")
+while not type in ["students inc", "courses only"]:
+    type = input("Incorrect input. Try again (students inc, courses only): ")
+
 # load data
-plotdata = pd.read_csv("sim_annealing_data.csv")
+if type == "students inc":
+    plotdata = pd.read_csv("../../results/scores_students_inc.csv")
+elif type == "courses only":
+    plotdata = pd.read_csv("../../results/scores_courses_only.csv")
 
-# plot histograms per simulated annealing type
-p1 = plt.hist(plotdata["sigmoidal(5000 iteraties)"], bins=50, fc=(1, 0, 0, 0.5), label = "Sigmoidal")
-p2 = plt.hist(plotdata["exponential(20000 iteraties)"], bins=50, fc=(0, 0, 1, 0.5), label = "Exponential")
-p3 = plt.hist(plotdata["linear(20000 iteraties)"], bins=50, fc=(0.8, 0.3, 0.5, 0.9), label = "Linear")
-p4 = plt.hist(plotdata["geman(20000 iteraties)"], bins=50, fc=(0.2, 1, 0.4, 0.9), label = "Geman")
-
-# create legend
-red_patch = mpatches.Patch(color=(1, 0, 0, 0.5), label="Sigmoidal")
-blue_patch = mpatches.Patch(color=(0, 0, 1, 0.5), label="Exponential")
-wine_patch = mpatches.Patch(color=(0.8, 0.3, 0.5, 0.9), label="Linear")
-new_patch = mpatches.Patch(color=(0.2, 1, 0.4, 0.9), label = "Geman")
-plt.legend(handles=[red_patch, blue_patch, wine_patch, new_patch])
-
-# add graph attributes
+# set graph attributes
 plt.xlabel("Score")
 plt.ylabel("Frequency")
-plt.title("Random scheduling: score distribution of simulated annealing types (N=25)")
+
+# plot random and hillclimber first
+p1 = plt.hist(plotdata["Random Scheduling"], bins=50, fc=(1, 0, 0, 0.5), label = "Random")
+p2 = plt.hist(plotdata["Random Hillclimber"], bins=50, fc=(0, 0, 1, 0.5), label = "Hillclimber")
+
+# create legend
+red_patch = mpatches.Patch(color=(1, 0, 0, 0.5), label="Random")
+blue_patch = mpatches.Patch(color=(0, 0, 1, 0.5), label="Hillclimber (1000 iteraties)")
+plt.legend(handles=[red_patch, blue_patch])
+
+# add graph title
+plt.title("Course scheduling: score distribution of random scheduling and hillclimber (N=1000)")
+
+# visualize plot
+plt.show()
+
+# plot histograms per simulated annealing type second
+p1 = plt.hist(plotdata["Sigmoidal"], bins=50, fc=(1, 0, 0, 0.5), label = "Sigmoidal")
+p2 = plt.hist(plotdata["Exponential"], bins=50, fc=(0, 0, 1, 0.5), label = "Exponential")
+p3 = plt.hist(plotdata["Linear"], bins=50, fc=(0.8, 0.3, 0.5, 0.5), label = "Linear")
+p4 = plt.hist(plotdata["Geman"], bins=50, fc=(0.2, 1, 0.4, 0.5), label = "Geman")
+
+# create legend
+red_patch = mpatches.Patch(color=(1, 0, 0, 0.5), label="Sigmoidal (5000 iteraties)")
+blue_patch = mpatches.Patch(color=(0, 0, 1, 0.5), label="Exponential (20000 iteraties)")
+wine_patch = mpatches.Patch(color=(0.8, 0.3, 0.5, 0.5), label="Linear (20000 iteraties)")
+new_patch = mpatches.Patch(color=(0.2, 1, 0.4, 0.5), label = "Geman (20000 iteraties)")
+plt.legend(handles=[red_patch, blue_patch, wine_patch, new_patch])
+
+# add graph title
+plt.title("Course scheduling: score distribution of simulated annealing types (N=1000)")
 
 # visualize plot
 plt.show()
